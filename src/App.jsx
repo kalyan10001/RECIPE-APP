00f4a483f4c41fc7b30554feb5867cd048e929f5
kalyan10001 <venkatakalyan1000@gmail.com>
2024-06-09 17:12:56 +0530

    ui completed

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Route, Routes } from "react-router-dom"
+import Sidebar from "./components/Sidebar"
+import FavouritesPage from "./pages/FavouritesPage"
+import HomePage from "./pages/HomePage"
+
+
+function App() {
+
+  return (
+    <div className="flex">
+      <Sidebar/>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/favourites" element={<FavouritesPage/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/FavouritesPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FavouritesPage = () => {
+  return (
+    <div className=''>
+      
+    </div>
+  )
+}
+
+export default FavouritesPage
